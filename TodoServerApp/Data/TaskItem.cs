4da6941de6809e976a617374a24bcff75f84128d
Danil Faizullin <toxen2005@mail.ru>
2024-10-05 14:16:18 +0500

    lection2

---
+++ TodoServerApp/Data/TaskItem.cs
@@ -0,0 +1,11 @@
+﻿namespace TodoServerApp.Data
+{
+    public class TaskItem
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; } //  ? - может быть Null
+        public string? Description { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public DateTime? FinishDate { get; set; }
+    }
+}
